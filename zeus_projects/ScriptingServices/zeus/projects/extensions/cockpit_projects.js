da3537f88d9a13a5119b429ef294bb0bcf741991
delchev <delchevn@gmail.com>
2017-03-01 12:09:31 +0200

    menu ext removed

--- zeus_projects/ScriptingServices/zeus/projects/extensions/cockpit_projects.js
+++ zeus_projects/ScriptingServices/zeus/projects/extensions/cockpit_projects.js
@@ -4,18 +4,20 @@
 const PATH = "/projects";
 const HTML_LINK = "../projects/index.html";
 
-exports.getMenuItem = function() {
-	return {  
-      "name": "Projects",
-      "path": PATH,
-      "link": HTML_LINK
-   };
-};
+//exports.getMenuItem = function() {
+//	return {  
+//      "name": "Projects",
+//      "path": PATH,
+//      "link": HTML_LINK
+//   };
+//};
 
 exports.getSidebarItem = function() {
 	return {  
       "name": "Projects",
       "path": PATH,
-      "link": HTML_LINK
+      "link": HTML_LINK,
+      "category": "Define",
+      "order": 103
    };
 };
